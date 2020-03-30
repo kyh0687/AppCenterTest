0e753354a765ff834a64c31c441cb1a36d9d17a0
김윤희 <kyh0687@gmail.com>
2020-03-30 17:51:30 +0900

    초기 커밋.

---
+++ App1/App.xaml.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class App : Application
+    {
+        const string LogTag = "AppCenterXamarinDemo";
+
+        const string androidKey = "b2a505d9-57c7-4a82-958a-eaaca2fc3259";
+
+        static App()
+        {
+            Crashes.SendingErrorReport += SendingErrorReportHandler;
+            Crashes.SentErrorReport += SentErrorReportHandler;
+            Crashes.FailedToSendErrorReport += FailedToSendErrorReportHandler;
+        }
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.LogLevel = LogLevel.Verbose;
+            Crashes.ShouldProcessErrorReport = ShouldProcess;
+            Crashes.ShouldAwaitUserConfirmation = ConfirmationHandler;
+            Crashes.GetErrorAttachments = GetErrorAttachments;
+            AppCenter.Start($"android={androidKey}",
+                               typeof(Analytics), typeof(Crashes));
+
+            AppCenter.GetInstallIdAsync().ContinueWith(installId =>
+            {
+                AppCenterLog.Info(LogTag, "AppCenter.InstallId=" + installId.Result);
+            });
+            Crashes.HasCrashedInLastSessionAsync().ContinueWith(hasCrashed =>
+            {
+                AppCenterLog.Info(LogTag, "Crashes.HasCrashedInLastSession=" + hasCrashed.Result);
+            });
+            Crashes.GetLastSessionCrashReportAsync().ContinueWith(report =>
+            {
+                AppCenterLog.Info(LogTag, "Crashes.LastSessionCrashReport.Exception=" + report.Result?.Exception);
+            });
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+
+       
+
+        static void SendingErrorReportHandler(object sender, SendingErrorReportEventArgs e)
+        {
+            AppCenterLog.Info(LogTag, "Sending error report");
+
+            var args = e as SendingErrorReportEventArgs;
+            ErrorReport report = args.Report;
+
+            //test some values
+            if (report.Exception != null)
+            {
+                AppCenterLog.Info(LogTag, report.Exception.ToString());
+            }
+            else if (report.AndroidDetails != null)
+            {
+                AppCenterLog.Info(LogTag, report.AndroidDetails.ThreadName);
+            }
+        }
+
+        static void SentErrorReportHandler(object sender, SentErrorReportEventArgs e)
+        {
+            AppCenterLog.Info(LogTag, "Sent error report");
+
+            var args = e as SentErrorReportEventArgs;
+            ErrorReport report = args.Report;
+
+            //test some values
+            if (report.Exception != null)
+            {
+                AppCenterLog.Info(LogTag, report.Exception.ToString());
+            }
+            else
+            {
+                AppCenterLog.Info(LogTag, "No system exception was found");
+            }
+
+            if (report.AndroidDetails != null)
+            {
+                AppCenterLog.Info(LogTag, report.AndroidDetails.ThreadName);
+            }
+        }
+
+        static void FailedToSendErrorReportHandler(object sender, FailedToSendErrorReportEventArgs e)
+        {
+            AppCenterLog.Info(LogTag, "Failed to send error report");
+
+            var args = e as FailedToSendErrorReportEventArgs;
+            ErrorReport report = args.Report;
+
+            //test some values
+            if (report.Exception != null)
+            {
+                AppCenterLog.Info(LogTag, report.Exception.ToString());
+            }
+            else if (report.AndroidDetails != null)
+            {
+                AppCenterLog.Info(LogTag, report.AndroidDetails.ThreadName);
+            }
+
+            if (e.Exception != null)
+            {
+                AppCenterLog.Info(LogTag, "There is an exception associated with the failure");
+            }
+        }
+
+        bool ShouldProcess(ErrorReport report)
+        {
+            AppCenterLog.Info(LogTag, "Determining whether to process error report");
+            return true;
+        }
+
+        bool ConfirmationHandler()
+        {
+            Xamarin.Forms.Device.BeginInvokeOnMainThread(() =>
+            {
+                Current.MainPage.DisplayActionSheet("Crash detected. Send anonymous crash report?", null, null, "Send", "Always Send", "Don't Send").ContinueWith((arg) =>
+                {
+                    var answer = arg.Result;
+                    UserConfirmation userConfirmationSelection;
+                    if (answer == "Send")
+                    {
+                        userConfirmationSelection = UserConfirmation.Send;
+                    }
+                    else if (answer == "Always Send")
+                    {
+                        userConfirmationSelection = UserConfirmation.AlwaysSend;
+                    }
+                    else
+                    {
+                        userConfirmationSelection = UserConfirmation.DontSend;
+                    }
+                    AppCenterLog.Debug(LogTag, "User selected confirmation option: \"" + answer + "\"");
+                    Crashes.NotifyUserConfirmation(userConfirmationSelection);
+                });
+            });
+
+            return true;
+        }
+
+        IEnumerable<ErrorAttachmentLog> GetErrorAttachments(ErrorReport report)
+        {
+            return new ErrorAttachmentLog[]
+            {
+                ErrorAttachmentLog.AttachmentWithText("Hello world!", "hello.txt"),
+                ErrorAttachmentLog.AttachmentWithBinary(Encoding.UTF8.GetBytes("Fake image"), "fake_image.jpeg", "image/jpeg")
+            };
+        }
+
+        
+    }
+}
---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace App1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            bool isEnabled = await Analytics.IsEnabledAsync();
+            Console.WriteLine(isEnabled);
+
+            Debug("debug - button click");
+
+
+
+            Error(new Exception("new exception!!!"));
+        }
+
+        public void Debug(string message)
+        {
+            Analytics.TrackEvent(nameof(Debug), new Dictionary<string, string> { { nameof(message), message } });
+        }
+
+        public void Warning(string message)
+        {
+            Analytics.TrackEvent(nameof(Warning), new Dictionary<string, string> { { nameof(message), message } });
+        }
+
+        public void Error(Exception exception)
+        {
+            Crashes.TrackError(exception);
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            // 크래시 발생 테스트 코드 
+            Crashes.GenerateTestCrash();
+        }
+    }
+}
